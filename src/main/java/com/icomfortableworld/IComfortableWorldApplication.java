420b5aabca075fc3c9c4ce8ab0895c6171cb1efb
devJWL <jwlee4450@gmail.com>
2024-02-23 18:14:07 +0900

    🎉 initial commit

---
+++ src/main/java/com/icomfortableworld/IComfortableWorldApplication.java
@@ -0,0 +1,13 @@
+package com.icomfortableworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IComfortableWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IComfortableWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icomfortableworld/IComfortableWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icomfortableworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IComfortableWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
