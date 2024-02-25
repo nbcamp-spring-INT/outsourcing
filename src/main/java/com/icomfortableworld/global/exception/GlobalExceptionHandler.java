c8215587d580facbc80ac3264542abdaf04e91e9
devJWL <jwlee4450@gmail.com>
2024-02-25 10:48:47 +0900

    ✨ feat(exception): jwt 관련 커스텀 예외처리 추가

--- src/main/java/com/icomfortableworld/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/icomfortableworld/global/exception/GlobalExceptionHandler.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.icomfortableworld.common.dto.CommonResponseDto;
+import com.icomfortableworld.global.exception.jwt.CustomJwtException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+	@ExceptionHandler(CustomJwtException.class)
+	public ResponseEntity<CommonResponseDto<Void>> customJwtExceptionHandler(CustomJwtException e) {
+		return CommonResponseDto.of(HttpStatus.UNAUTHORIZED, e.getMessage(), null);
+	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<CommonResponseDto<Void>> methodArgumentNotValidException(
