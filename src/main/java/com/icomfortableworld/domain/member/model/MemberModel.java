81d1686420f6d428b736210d5e3d3e5ac8145b89
devJWL <jwlee4450@gmail.com>
2024-02-25 16:32:07 +0900

    ✨ feat(member): member 관련 추가 기능
    필드 생성, @Getter, @Builder, @NoArgsConstructor,@AllArgsConstructor 추가

--- src/main/java/com/icomfortableworld/domain/member/model/MemberModel.java
+++ src/main/java/com/icomfortableworld/domain/member/model/MemberModel.java
@@ -1,4 +1,20 @@
 package com.icomfortableworld.domain.member.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberModel {
+	private Long memberId;
+	private String identity;
+	private String username;
+	private String nickname;
+	private String password;
+	private String introduction;
+	private MemberRole memberRole;
 }
