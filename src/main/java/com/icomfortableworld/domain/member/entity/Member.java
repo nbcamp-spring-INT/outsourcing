daf2e825ec8db9f48ab65dc59945b373451b99a8
devJWL <jwlee4450@gmail.com>
2024-02-25 16:30:50 +0900

    ✨ feat(member): member 관련 추가 기능
    @Builder 추가
    Timestamped 상속
    model 타입으로 반환하는 함수 toModel 추가
    soft delete를 위한 @SQLDelete, @SQLRestriction 추가

--- src/main/java/com/icomfortableworld/domain/member/entity/Member.java
+++ src/main/java/com/icomfortableworld/domain/member/entity/Member.java
@@ -1,19 +1,32 @@
 package com.icomfortableworld.domain.member.entity;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import com.icomfortableworld.common.entity.Timestamped;
+import com.icomfortableworld.domain.member.model.MemberModel;
 import com.icomfortableworld.domain.member.model.MemberRole;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@Builder
+@SQLDelete(sql = "update member set deleted_date = NOW() where id = ?")
+@SQLRestriction(value = "deleted_date is NULL")
 @NoArgsConstructor
 @AllArgsConstructor
-public class Member {
+public class Member extends Timestamped {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,6 +41,19 @@ public class Member {
 	private String password;
 	@Column(length = 40)
 	private String introduction;
-	@Column(nullable = false)
-	private MemberRole role;
+	@Column(nullable = false, length = 10)
+	@Enumerated(value = EnumType.STRING)
+	private MemberRole memberRole;
+
+	public MemberModel toModel() {
+		return MemberModel.builder()
+			.memberId(memberId)
+			.identity(identity)
+			.username(username)
+			.nickname(nickname)
+			.password(password)
+			.introduction(introduction)
+			.memberRole(memberRole)
+			.build();
+	}
 }
