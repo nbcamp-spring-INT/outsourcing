2b5ac1eda1cc2fdb2bb5062cec1869a03b86ca87
kimpangya <nam74415133@gmail.com>
2024-02-26 15:43:45 +0900

    🎨 feat(feed) : FeedRepository에 애노테이션 추가

--- src/main/java/com/icomfortableworld/domain/feed/repository/FeedRepositoryImpl.java
+++ src/main/java/com/icomfortableworld/domain/feed/repository/FeedRepositoryImpl.java
@@ -1,4 +1,7 @@
 package com.icomfortableworld.domain.feed.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class FeedRepositoryImpl implements FeedRepository{
 }
