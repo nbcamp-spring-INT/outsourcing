eebc95031d1f143be86c6713c686d434df589761
jiwon317 <chlghqlt12@naver.com>
2024-02-26 15:31:38 +0900

    pull 용 커밋

--- src/main/java/com/icomfortableworld/domain/comment/entity/Comment.java
+++ src/main/java/com/icomfortableworld/domain/comment/entity/Comment.java
@@ -1,4 +1,24 @@
 package com.icomfortableworld.domain.comment.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Table(name = "comments")
 public class Comment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long commentId;
+
+
+
 }
