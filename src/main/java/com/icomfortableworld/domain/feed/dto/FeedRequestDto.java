c2d360eaab27c311fb701095b1b6cf623b633094
kimpangya <nam74415133@gmail.com>
2024-02-27 01:25:26 +0900

    ✨ feat(feeds) : Tag & TagSet createFeed 구현 완료

--- src/main/java/com/icomfortableworld/domain/feed/dto/FeedRequestDto.java
+++ src/main/java/com/icomfortableworld/domain/feed/dto/FeedRequestDto.java
@@ -1,16 +1,20 @@
 package com.icomfortableworld.domain.feed.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.icomfortableworld.common.entity.Timestamped;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class FeedRequestDto extends Timestamped {
+public class FeedRequestDto{
 	@NotBlank(message = "내용이 없습니다.")
 	private String content;
+
+	private List<String> tagNameList;
 }
--- src/main/java/com/icomfortableworld/domain/feed/service/FeedServiceImpl.java
+++ src/main/java/com/icomfortableworld/domain/feed/service/FeedServiceImpl.java
@@ -1,15 +1,21 @@
 package com.icomfortableworld.domain.feed.service;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.icomfortableworld.domain.feed.dto.FeedRequestDto;
 import com.icomfortableworld.domain.feed.entity.Feed;
+import com.icomfortableworld.domain.feed.model.FeedModel;
+import com.icomfortableworld.domain.tag.entity.Tag;
+import com.icomfortableworld.domain.tag.entity.TagSet;
 import com.icomfortableworld.domain.feed.repository.FeedRepository;
 import com.icomfortableworld.domain.member.entity.Member;
+import com.icomfortableworld.domain.member.repository.MemberRepository;
+import com.icomfortableworld.domain.tag.repository.TagRepository;
+import com.icomfortableworld.domain.tag.repository.TagSetRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,10 +25,26 @@ import lombok.RequiredArgsConstructor;
 public class FeedServiceImpl implements FeedService{
 
 	private final FeedRepository feedRepository;
+	private final MemberRepository memberRepository;
+	private final TagRepository tagRepository;
+	private final TagSetRepository tagSetRepository;
 
+
+	//유저 존재 확인
+	//태그 같이 생성
 	@Override
 	public void createFeed(FeedRequestDto requestDto, Long memberId) {
-		Feed feed = new Feed(requestDto, memberId);
-		feedRepository.save(feed);
+		memberRepository.findByIdOrElseThrow(memberId);
+		FeedModel feedModel = feedRepository.save(new Feed(requestDto, memberId)).toModel();
+
+		for(String tagName : requestDto.getTagNameList()){
+			//없으면 만들어주기~
+			TagSet tagSet = tagSetRepository.findByTagName(tagName).orElse(null);
+			if(tagSet==null){
+				tagSet = tagSetRepository.save(new TagSet(tagName));
+			}
+			Tag tag = new Tag(feedModel.getFeedId(), tagSet.getTagSetId());
+			tagRepository.save(tag);
+		}
 	}
 }
