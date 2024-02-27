ed83c6d36e0835a5a8573778f5f8a249b0e67fbe
kimpangya <97017924+kimpangya@users.noreply.github.com>
2024-02-28 01:21:37 +0900

    feedService 파일 삭제
    feedService파일 있어서 삭제했습니다

--- src/main/java/com/icomfortableworld/domain/feed/service/FeedService.java
+++
@@ -1,7 +0,0 @@
-package com.icomfortableworld.domain.feed.service;
-
-import com.icomfortableworld.domain.feed.dto.FeedRequestDto;
-
-public interface FeedService {
-    void createFeed(FeedRequestDto requestDto, Long memberId);
-}
