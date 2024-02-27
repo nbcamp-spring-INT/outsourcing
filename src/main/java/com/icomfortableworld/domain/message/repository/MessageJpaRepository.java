1159b1202dfd72e5bb359da1c25eeb204dfbcf02
jehyun <jehyun1590@naver.com>
2024-02-27 16:35:52 +0900

    :sparkles: feat(message):메세지 보내기 및 메세지 조회

---
+++ src/main/java/com/icomfortableworld/domain/message/repository/MessageJpaRepository.java
@@ -0,0 +1,11 @@
+package com.icomfortableworld.domain.message.repository;
+
+import com.icomfortableworld.domain.message.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageJpaRepository extends JpaRepository <Message,Long> {
+    List<Message> findByReceiverId(Long receiverId);
+
+}
---
+++ src/main/java/com/icomfortableworld/domain/message/service/MessageService.java
@@ -0,0 +1,12 @@
+package com.icomfortableworld.domain.message.service;
+
+import com.icomfortableworld.domain.message.dto.request.MessageRequestDto;
+import com.icomfortableworld.domain.message.dto.response.MessageResponseDto;
+import java.util.List;
+
+public interface MessageService {
+    void sendMessage(MessageRequestDto requestDto, Long memberId);
+
+    List<MessageResponseDto> getMessageList(Long memberId);
+
+}
