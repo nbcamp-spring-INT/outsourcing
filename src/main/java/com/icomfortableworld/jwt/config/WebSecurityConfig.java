ecaf6803874b56575987b19026754f339431afea
devJWL <jwlee4450@gmail.com>
2024-02-28 21:46:43 +0900

    ✨ feat(Swagger): 스웨거 설정

--- src/main/java/com/icomfortableworld/jwt/config/WebSecurityConfig.java
+++ src/main/java/com/icomfortableworld/jwt/config/WebSecurityConfig.java
@@ -50,6 +50,9 @@ public class WebSecurityConfig {
 				.requestMatchers("/").permitAll()
 				.requestMatchers("/api/version-1/members/login").permitAll()
 				.requestMatchers("/api/version-1/members/signup").permitAll()
+				.requestMatchers("/swagger-ui/**").permitAll()
+				.requestMatchers("/swagger-ui.html").permitAll()
+				.requestMatchers("/v3/api-docs/**").permitAll()
 				.anyRequest().authenticated()
 		);
 
