14280e6e29198b11165f82eb5f92c74711b932b2
devJWL <jwlee4450@gmail.com>
2024-02-28 14:23:15 +0900

    🐛 refactor(member): requsetDto @Valid 설정

--- src/main/java/com/icomfortableworld/domain/member/dto/request/LoginRequestDto.java
+++ src/main/java/com/icomfortableworld/domain/member/dto/request/LoginRequestDto.java
@@ -1,10 +1,10 @@
 package com.icomfortableworld.domain.member.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
+@AllArgsConstructor
 public class LoginRequestDto {
 	private String username;
 	private String password;
--- src/main/java/com/icomfortableworld/domain/member/dto/request/SignupRequestDto.java
+++ src/main/java/com/icomfortableworld/domain/member/dto/request/SignupRequestDto.java
@@ -2,13 +2,18 @@ package com.icomfortableworld.domain.member.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
+@AllArgsConstructor
 public class SignupRequestDto {
-	@NotBlank
+	@NotBlank(message = "username은 공백일 수 없습니다.")
+	@Pattern(regexp = "^(?=.*[a-z]).+$", message = "username은 알파벳 소문자가 포함되어야 합니다.")
+	@Pattern(regexp = "^(?=.*\\d).+$", message = "username 숫자가 포함되어야 합니다.")
+	@Size(min = 4, max = 10)
 	private String username;
 
 	@Email
@@ -17,7 +22,12 @@ public class SignupRequestDto {
 
 	@NotBlank
 	private String nickname;
-	@NotBlank
+	@NotBlank(message = "password는 공백일 수 없습니다.")
+	@Pattern(regexp = "^(?=.*[a-z]).+$", message = "비밀번호는 알파벳 소문자가 포함되어야 합니다.")
+	@Pattern(regexp = "^(?=.*[A-Z]).+$", message = "비밀번호는 알파벳 대문자가 포함되어야 합니다.")
+	@Pattern(regexp = "^(?=.*\\d).+$", message = "비밀번호는 숫자가 포함되어야 합니다.")
+	@Pattern(regexp = "^(?=.*[!@#$%^&*(){}]).+$", message = "비밀번호는 특수문자가 포함되어야 합니다.")
+	@Size(min = 8, max = 15)
 	private String password;
 
 	private String introduction;
